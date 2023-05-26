811a66ed75c8917beeef2876315ebb3409505309
Amory <theonlygeeking@gmail.com>
2023-05-26 21:09:57 +0200

    feat(plugins): add todo-comments and smartcolumn

---
+++ plugins/smartcolumn.lua
@@ -0,0 +1,16 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  lazy = false,
+  opts = {
+    colorcolumn = "100",
+    disabled_filetypes = {
+      "help",
+      "text",
+      "markdown",
+      "NvimTree",
+      "lazy",
+      "mason",
+      "alpha",
+    },
+  },
+}
